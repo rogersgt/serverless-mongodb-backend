193240546cb3f47f9b112ae7a4fa8fa010a70d44
rogersgt <gradytrogers@gmail.com>
2019-06-10 21:46:20 -0400

    yarn

--- setup/index.js
+++ setup/index.js
@@ -1,57 +1,11 @@
-require('colors');
-const fs = require('fs');
-const dotenv = require('dotenv');
-const genPwd = require('generate-password');
-const { MongoClient } = require('mongodb');
+const Rhinocloud = require('rhinocloud-sdk');
 
-const SETUP_ENV = `${__dirname}/../setup.env`;
+const rc = new Rhinocloud();
 
-if (fs.existsSync(SETUP_ENV)) {
-  dotenv.config({ path: SETUP_ENV });
-}
-
-const {
-  MONGO_URI = 'mongodb://127.0.0.1:27017',
-  DB_NAME = 'db',
-  API_USER = 'api',
-  API_PASSWORD = genPwd.generate({ numbers: true, length: 10 })
-} = process.env;
-
-(async function initializeDB() {
-  let conn;
-
+(async function deploy() {
   try {
-    conn = await MongoClient.connect(MONGO_URI, { useNewUrlParser: true });
-
-    const appDB = conn.db(DB_NAME);
-    await appDB.addUser(API_USER, API_PASSWORD, {
-      roles: [{
-        role: 'readWrite',
-        db: DB_NAME
-      }]
-    });
-    conn.close();
-    console.log(`
-      Serverless API Credentials.
-    `.gray,
-    `
-    **RETAIN THESE FOR FUTURE DEPLOYMENTS**
-    `.red,
-    `username:`.gray,
-    `${API_USER}`.green,
-    `password:`.gray,
-    `${API_PASSWORD}`.green,
-    `
-    Include the connection uri in your env.json. Don't forget to replace the placeholders with
-    your mongo host and port.
-    {
-      "MONGO_URI": "mongodb://${API_USER}:${API_PASSWORD}@<your mongo host>:<your mongo port>/${DB_NAME}"
-    }
-    `
-    );
+    
   } catch (error) {
-    console.log(error);
-    conn.close();
-    process.exit(1);
+    
   }
 })();
